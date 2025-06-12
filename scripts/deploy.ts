be30b258238021bf15ab7bee799224756aba9a15
ivanovjju <ivanovvjju@gmail.com>
2025-06-12 19:23:02 +0400

    Create deploy to sepolia

---
+++ scripts/deploy.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const TokenVault = await ethers.getContractFactory("TokenVault");
+    const vault = await TokenVault.deploy();
+    await vault.waitForDeployment();
+
+    console.log(`Deployed to ${await vault.getAddress()}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
